afde2a06fac555c83aff6d45b918bea30bccea6c
Khoa Le <khoalt25791@gmail.com>
2018-04-19 16:54:56 +0700

    Set up project

---
+++ config/deploy/production.rb
@@ -0,0 +1,4 @@
+server "206.189.38.24",
+       user: "khoale",
+       roles: %w(web app db),
+       primary: true)
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  mount Sidekiq::Web => "/sidekiq" # monitoring console
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
