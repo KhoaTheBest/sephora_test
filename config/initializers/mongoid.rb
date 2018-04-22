2f2c76165d0a963652e6ae2ccef893636711fa92
Khoa Le <khoalt25791@gmail.com>
2018-04-22 10:15:59 +0700

    - Create Product Model / Controller - Implemented index/show methods in Controller - Applied Rspec test for controller

---
+++ config/initializers/mongoid.rb
@@ -0,0 +1,7 @@
+module BSON
+  # ObjectId
+  class ObjectId
+    alias to_json to_s
+    alias as_json to_s
+  end
+end
---
+++ spec/support/api_helper.rb
@@ -0,0 +1,8 @@
+# ApiHelper
+module ApiHelper
+  include Rack::Test::Methods
+
+  def app
+    Rails.application
+  end
+end
